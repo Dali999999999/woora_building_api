449bc248ef21ee67dc190a969aa6333b7915905e
Dali999999999 <dalinigba@gmail.com>
2025-07-29 10:38:18 +0100

    Update schemas.py

---
+++ woora_api/app/schemas.py
@@ -0,0 +1,9 @@
+from marshmallow import Schema, fields
+
+class VisitSettingsSchema(Schema):
+    """
+    Schéma pour la validation et la sérialisation des paramètres de visite.
+    """
+    initial_free_visit_passes = fields.Int(required=True, description="Nombre de pass de visite gratuits offerts à l'inscription.")
+    visit_pass_price = fields.Decimal(places=2, required=True, description="Prix d'un seul pass de visite lors de l'achat.")
+
