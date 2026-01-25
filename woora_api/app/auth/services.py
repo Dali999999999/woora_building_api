9167a8609f8e9ed8f3e0c164372f2d2e29fd8364
dalini <dalinigba@gmail.com>
2026-01-25 10:05:59 +0100

    fix

--- woora_api/app/auth/services.py
+++ woora_api/app/auth/services.py
@@ -20,7 +20,7 @@ def generate_verification_code():
     return ''.join(random.choices(string.digits, k=6))
 
 def send_verification_email(email, code):
-    msg = Message('Code de Vérification Woora Immo',
+    msg = Message('Code de Vérification Woora Building',
                     sender=current_app.config['MAIL_DEFAULT_SENDER'],
                     recipients=[email])
     msg.body = f'Votre code de vérification est : {code}. Ce code est valide pendant 60 secondes.'
